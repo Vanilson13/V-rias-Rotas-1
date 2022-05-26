26686f2f4aa4a694068bc54914d186b74467fe38
Vandilson13 <101371663+Vanilson13@users.noreply.github.com>
2022-05-26 23:52:15 +0000

    certo

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def index():
 @app.route('/unifran')
 def unifra():
     return '<h2>Universidade de Franca</h2>:'
-
+  
 @app.route('/dashboard/Eduardo')
-def eduardo():
-    return '<h3>Olá, Eduardo</h3>'
-
-if __name__ == '__main__':
+def dashboard():
+    return '<h3>Olá Eduardo</h3>:'
+  
+if __name__ =='__main__':
   app.run(host='0.0.0.0', port=8080)
